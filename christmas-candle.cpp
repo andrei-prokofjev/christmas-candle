b78b95fee4c22fc318093a29b55de27f2e576dc7
andrei <andrei@andrei-vs>
2018-11-23 16:21:01 -0500

    gg

--- christmas-candle.cpp
+++ christmas-candle.cpp
@@ -8,15 +8,15 @@
 
 Wdt wdt;
 
-uint8_t led1 = 1;
-uint8_t led2 = 1;
-uint8_t led3 = 1;
-uint8_t led4 = 1;
-uint8_t led5 = 1;
-uint8_t led6 = 1;
-uint8_t led7 = 1;
-uint8_t led8 = 1;
-uint8_t led9 = 1;
+uint8_t led1 = 7;
+uint8_t led2 = 6;
+uint8_t led3 = 5;
+uint8_t led4 = 8;
+uint8_t led5 = 4;
+uint8_t led6 = 9;
+uint8_t led7 = 10;
+uint8_t led8 = 11;
+uint8_t led9 = 12;
 
 uint8_t candles[] = { led1, led2, led3, led4, led5, led6, led7, led8, led9 };
 
@@ -26,24 +26,29 @@ void setup() {
 	delay(100);
 	pinMode(LED_BUILTIN, OUTPUT);
 
+	digitalWrite(LED_BUILTIN, LOW);
+
 	for (int i = 0; i < sizeof(candles); i++) {
 		pinMode(candles[i], OUTPUT);
-		analogWrite(candles[i], 0);
 	}
 
 	Serial.println("Initialization complete.");
 	delay(100);
 
 	say_hello();
+
 }
 
 void loop() {
+
 	if (wdt.isSleep()) {
 		return;
 	}
 
 	blink(20);
 
+
+//
 	int val = analogRead(SENSOR_PIN);
 	Serial.println(val);
 
@@ -51,6 +56,14 @@ void loop() {
 }
 
 void say_hello() {
-
+	for (int i = 0; i < sizeof(candles); i++) {
+		delay(30);
+		digitalWrite(candles[i], HIGH);
+	}
+	delay(100);
+	for (int i = 0; i < sizeof(candles); i++) {
+			delay(30);
+			digitalWrite(candles[i], LOW);
+		}
 }
 
